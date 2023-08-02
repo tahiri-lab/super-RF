917f57c66808edcb06a45887d67f9954149bb4cf
Arthur Debeaupte <arthurdebeaupte@Arthurs-MacBook-Pro-2.local>
2023-08-02 13:39:52 -0400

    implementing new approach to get bipartitions

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,6 @@
 #include "parser.hpp"
 
-
+/**
 vector<string> parseStage(string stageString) {
     //newickString = "(ant:17, (bat:31, cow:22):7, dog:22, (elk:33, fox:12):40)";
 
@@ -140,3 +140,150 @@ void printVectorUtil(vector<vector<string>> vectorInput) {
 //TODO function to count number of elements in whole Newick for it to stop the recursion
 //TODO when all steps analyzed, recursion is stoped
 //TODO find a tuto to create recursion functions
+
+ */
+
+vector<string> getStringStackBipartitions(string inputString) {
+    vector<string> result;
+    stack<string> stack;
+    string subString = "";
+
+    for(char c : inputString) {
+        if(c == '(') {
+            subString = "";
+            stack.push(subString);
+        } else if (c == ')') {
+            result.push_back(stack.top());
+            stack.pop();
+        } else {
+            subString += c;
+            if (!stack.empty()) {
+                stack.top() = (stack.top() += c);
+            }
+        }
+    }
+    return result;
+}
+
+/**
+ * @brief deletes a taxon distance
+ *
+ * @param inputString a taxon with its distance : "cat:13"
+ * @return taxon without its distance : "cat"
+ * */
+string deleteTaxonDistance(string inputString) {
+    string result;
+
+    for(char c: inputString) {
+        if (c == ':'){
+            break;
+        }
+        result += c;
+    }
+    return result;
+}
+
+/**
+ * @brief splits a string in Newick format such as bat:31, cow:22, goat:21, bird:12 into a vector<string>
+ *
+ * @param inputString newick format string : "bat:31, cow:22, goat:21, bird:12"
+ * @return vector<string> : bat cow goat bird
+ * */
+vector<string> splitStringToVector(string inputString) {
+    vector<string> result;
+    string subString;
+
+    for(char c: inputString) {
+        if(c == ',' && subString != "") {
+            if (deleteTaxonDistance(subString) != "") {
+                result.push_back(deleteTaxonDistance(subString));
+            }
+            subString = "";
+        } else if (c != ' '){
+            subString += c;
+        }
+    }
+    if ((deleteTaxonDistance(subString) != "")) {
+        result.push_back(deleteTaxonDistance(subString));
+    }
+    return result;
+}
+
+/**
+ * @brief gets all taxons of a phylogenetic tree
+ *
+ * @param inputString newick representation of the tree
+ * @return vector<string> where each string is one of the tree's taxon
+ * */
+vector<string> getAllTaxons(string inputString) {
+    vector<string> result;
+    vector<string> stringBipartitions = getStringStackBipartitions(inputString);
+    vector<string> currentVector;
+
+    for (string s: stringBipartitions) {
+        currentVector = splitStringToVector(s);
+        result.insert(result.end(), currentVector.begin(), currentVector.end());
+    }
+    return result;
+}
+
+/**
+ * @brief creates a pair<list<string>, list<string>> bipartition
+ *
+ * @param allSpecies a vector<string> with all taxons from a tree
+ * @param currentBipartition a vector<string> with a tree bipartition's species
+ *
+ * @return a pair<list<string>, list<string>> corresponding to a bipartition, each list being a side of the tree
+ *         bipartition
+ * */
+pair<list<string>, list<string>> createPairBipartition(vector<string> allSpecies, vector<string> currentBipartition) {
+    pair<list<string>, list<string>> result;
+    list<string> list1;
+    list<string> list2;
+
+    for(string s: currentBipartition) {
+        list1.push_back(s);
+    }
+
+    for(string s: allSpecies) {
+        if (!findStringInList(list1, s)) {
+            list2.push_back(s);
+        }
+    }
+
+    result.first = list1;
+    result.second = list2;
+
+    return result;
+}
+
+/**
+ * @brief checks if a string is present in a list<string>
+ *
+ * @param list the list
+ * @param target the string target
+ *
+ * @return true if target found
+ * */
+bool findStringInList(list<string> list, string target) {
+    bool result = false;
+    for (string s: list) {
+        if(s == target) {
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+
+
+/**
+ * @brief gets all bipartions of a phylogenetical tree in Newick representation
+ *
+ * @param newickInputString newick representation of the phylogenetic tree
+ * @return <vector<pair<list<string>, list<string>>> where each pair corresponds to a bipartition
+ * */
+vector<pair<list<string>, list<string>>> getBipartitions(string newickInputString) {
+
+}
--- parser.hpp
+++ parser.hpp
@@ -9,8 +9,12 @@
 #include <string>
 #include <utility>
 #include <list>
+#include <stack>
+#include <algorithm>
 using namespace std;
 
+
+/**
 vector<string> parseStage(string stageString);
 string deleteSpaces(string inputString);
 string cleanTaxon(string taxon);
@@ -19,6 +23,26 @@ vector<string> getStageElements(string str);
 vector<pair<list<string>, list<string>>> stageBipartitioner(vector<vector<string>> stageVector);
 pair<list<string>, list<string>> cutStringVector(vector<vector<string>> vector, int cutIndex);
 void printVectorUtil(vector<vector<string>> vectorInput);
+
+*/
+
+
+vector<string> getStringStackBipartitions(string inputString);
+
+string deleteTaxonDistance(string inputString);
+
+vector<string> splitStringToVector(string inputString);
+
+vector<string> getAllTaxons(string inputString);
+
+pair<list<string>, list<string>> createPairBipartition(vector<string> allSpecies, vector<string> currentBipartition);
+
+vector<pair<list<string>, list<string>>> getBipartitions(string newickInputString);
+
+bool findStringInList(list<string> list, string target);
+
+
+
 
 
 #endif //NEWICKBIPARTITIONPARSER__PARSER_HPP
