ee88eddf1ce08335c719f28f819b66cf3bdc677e
Arthur Debeaupte <arthurdebeaupte@Arthurs-MacBook-Pro-2.local>
2023-08-07 12:15:06 -0400

    creating bipartitions.ccp, a lib to manipulate bipartitions

---
+++ include/bipartitions.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Arthur Debeaupte on 07/08/2023.
+//
+
+#ifndef NEWICKBIPARTITIONPARSER__BIPARTITIONS_HPP
+#define NEWICKBIPARTITIONPARSER__BIPARTITIONS_HPP
+
+#include <iostream>
+#include <string>
+#include <utility>
+#include <list>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+
+int getBipartitionSize(pair<list<string>, list<string>> inputBipartition);
+
+#endif //NEWICKBIPARTITIONPARSER__BIPARTITIONS_HPP
---
+++ src/bipartitions.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Arthur Debeaupte on 07/08/2023.
+//
+
+#include "../include/bipartitions.hpp"
+
+int getBipartitionSize(pair<list<string>, list<string>> inputBipartition) {
+    
+}
